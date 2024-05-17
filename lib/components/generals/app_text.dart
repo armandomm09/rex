1c59b543161bdcf9871af5e4882922750da61a44
binivazqua <binizavazquez@gmail.com>
2024-05-17 12:27:53 -0600

    pedillos con el vertical layout

--- lib/components/generals/app_text.dart
+++ lib/components/generals/app_text.dart
@@ -1,56 +1,61 @@
-
 import 'package:flutter/material.dart';
 
-enum TextColor {
-  red, black, gray, blueAlliance, redAlliance
-}
+enum TextColor { red, black, gray, blueAlliance, redAlliance, base }
 
 class AppText extends StatelessWidget {
   final String text;
   final TextColor textColor;
   final double fontSize;
-  const AppText({super.key, required this.text, this.textColor = TextColor.black, this.fontSize = 18});
+  //final String font;
+  const AppText({
+    super.key,
+    required this.text,
+    this.textColor = TextColor.black,
+    this.fontSize = 18,
+    //required this.font});
+  });
+
+  getColor(BuildContext context) {
+    if (textColor == TextColor.red) {
+      return Theme.of(context).colorScheme.inversePrimary;
+    } else if (textColor == TextColor.black) {
+      return Theme.of(context).colorScheme.surface;
+    } else if (textColor == TextColor.gray) {
+      return Theme.of(context).colorScheme.secondary;
+    } else if (textColor == TextColor.blueAlliance) {
+      return const Color.fromARGB(255, 31, 49, 151);
+    } else if (textColor == TextColor.redAlliance) {
+      return const Color.fromARGB(255, 220, 8, 8);
+    } else if (textColor == TextColor.base) {
+      return const Color.fromARGB(255, 255, 255, 255);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-
-    getColor(){
-    if(textColor == TextColor.red){
-      return Theme.of(context).colorScheme.inversePrimary;
-    } else if(textColor == TextColor.black){
-      return Theme.of(context).colorScheme.surface;
-    } else if(textColor == TextColor.gray){
-      return Theme.of(context).colorScheme.secondary;
-    } else if(textColor == TextColor.blueAlliance){
-      return const Color.fromARGB(255, 31, 49, 151);
-    } else if(textColor == TextColor.redAlliance){
-      return const Color.fromARGB(255, 220, 8, 8);
-    }
-  }
     return Text(
-                text,
-                //selectionHeightStyle: BoxHeightStyle.includeLineSpacingBottom,
-                style: TextStyle(
-                    color: getColor(),
-                    fontSize: fontSize,
-                    fontFamily: "Industry",
-                    fontWeight: FontWeight.bold,
-                    fontStyle: FontStyle.italic),
-              );
+      text,
+      //selectionHeightStyle: BoxHeightStyle.includeLineSpacingBottom,
+      style: TextStyle(
+        color: getColor(context),
+        fontSize: fontSize,
+        fontFamily: 'Inter',
+      ),
+    );
   }
 
-  getTextStyle(TextColor color, BuildContext context){
-    Color colorForText;
-    if(color == TextColor.red){
-      colorForText = Theme.of(context).colorScheme.inversePrimary;
-    } else {
-      colorForText = Theme.of(context).colorScheme.surface;
-    }
-    return TextStyle(
-                    color: colorForText ,
-                    fontSize: fontSize,
-                    fontFamily: "Industry",
-                    fontWeight: FontWeight.bold,
-                    fontStyle: FontStyle.italic);
-  }
+  // getTextStyle(TextColor color, BuildContext context) {
+  //   Color colorForText = getColor;
+  //   if (color == TextColor.red) {
+  //     colorForText = Theme.of(context).colorScheme.inversePrimary;
+  //   } else {
+  //     colorForText = Theme.of(context).colorScheme.surface;
+  //   }
+  //   return TextStyle(
+  //       color: ,
+  //       fontSize: fontSize,
+  //       fontFamily: "Inter",
+  //       fontWeight: FontWeight.bold,
+  //       fontStyle: FontStyle.italic);
+  // }
 }
--- lib/services/openai/scout_gpt.dart
+++ lib/services/openai/scout_gpt.dart
@@ -6,24 +6,22 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:ssh2/ssh2.dart';
 import 'package:uuid/uuid.dart';
 
-class ScoutGPTService{
-
+class ScoutGPTService {
   static final FirebaseFirestore firestore = FirebaseFirestore.instance;
   static final FirebaseAuth auth = FirebaseAuth.instance;
   static final String currentUserID = auth.currentUser!.uid;
-  static final String currentUserEmail= auth.currentUser!.email!;
-
+  static final String currentUserEmail = auth.currentUser!.email!;
 
   static newGptMessage(String question) async {
-
-    final GptMessage message = GptMessage(currentUserID, currentUserEmail, question, Timestamp.now(), const Uuid().v1());
+    final GptMessage message = GptMessage(currentUserID, currentUserEmail,
+        question, Timestamp.now(), const Uuid().v1());
 
     try {
       await firestore
-      .collection('Users')
-      .doc(currentUserID)
-      .collection('gptChat')
-      .add(message.toMap());
+          .collection('Users')
+          .doc(currentUserID)
+          .collection('gptChat')
+          .add(message.toMap());
       print('Sent succesfull');
       //newGPTAnswer(question);
       newGPTAnswer(question);
@@ -33,17 +31,18 @@ class ScoutGPTService{
       return PostState.serverError;
     }
   }
-  static Future<String> askGPT(String question) async {
 
+  static Future<String> askGPT(String question) async {
     final ssh = SSHClient(
-      host: sshHost,
-      port: sshPort, 
-      username: sshUsername, 
-      passwordOrKey: sshPassword);
+        host: sshHost,
+        port: sshPort,
+        username: sshUsername,
+        passwordOrKey: sshPassword);
 
     try {
       await ssh.connect();
-      String? result = await ssh.execute('python3 scouting/scout_gpt/run.py "$question"');
+      String? result =
+          await ssh.execute('python3 scouting/scout_gpt/run.py "$question"');
       return result!;
     } catch (e) {
       return 'Sorry, there was a problem: $e';
@@ -53,50 +52,46 @@ class ScoutGPTService{
   static newGPTAnswer(String question) async {
     try {
       String loadingMessageUuid = const Uuid().v1();
-      GptMessage loadingMessage = GptMessage('chatGPT', 'chatGPT@imperator.com', "-LOADING-", Timestamp.now(), loadingMessageUuid);
+      GptMessage loadingMessage = GptMessage('chatGPT', 'chatGPT@imperator.com',
+          "-LOADING-", Timestamp.now(), loadingMessageUuid);
       await firestore
-      .collection('Users')
-      .doc(currentUserID)
-      .collection('gptChat')
-      .add(loadingMessage.toMap());
+          .collection('Users')
+          .doc(currentUserID)
+          .collection('gptChat')
+          .add(loadingMessage.toMap());
 
       String answer = await askGPT(question);
 
-      GptMessage message = GptMessage('chatGPT', 'chatGPT@imperator.com', answer.trim(), Timestamp.now(), loadingMessageUuid);
+      GptMessage message = GptMessage('chatGPT', 'chatGPT@imperator.com',
+          answer.trim(), Timestamp.now(), loadingMessageUuid);
 
       var docsWithUuid = await firestore
-      .collection('Users')
-      .doc(currentUserID)
-      .collection('gptChat')
-      .where('uuid', isEqualTo: loadingMessageUuid)
-      .limit(1)
-      .get();
+          .collection('Users')
+          .doc(currentUserID)
+          .collection('gptChat')
+          .where('uuid', isEqualTo: loadingMessageUuid)
+          .limit(1)
+          .get();
 
       docsWithUuid.docs.forEach((doc) async {
         await firestore
-      .collection('Users')
-      .doc(currentUserID)
-      .collection('gptChat')
-      .doc(doc.id)
-      .update(message.toMap());
+            .collection('Users')
+            .doc(currentUserID)
+            .collection('gptChat')
+            .doc(doc.id)
+            .update(message.toMap());
       });
-      
-      
     } catch (e) {
       print('Error on newGPTAnswer at $e');
     }
-    
   }
 
   static Stream<QuerySnapshot> getMessages() {
-
-
     return firestore
-          .collection("Users")
-          .doc(currentUserID)
-          .collection("gptChat")
-          .orderBy("timestamp", descending: false)
-          .snapshots();
+        .collection("Users")
+        .doc(currentUserID)
+        .collection("gptChat")
+        .orderBy("timestamp", descending: false)
+        .snapshots();
   }
-  
 }
