4bf8af20c1a2d6c5442a29beacde9b934d56fa4b
Armando <pabloarmando.macbeath@gmail.com>
2024-09-24 23:34:14 -0600

    Update tba_event.dart

--- lib/services/TBA/tba_event.dart
+++ lib/services/TBA/tba_event.dart
@@ -2,8 +2,29 @@
 
 
 class TBAEventService {
-  var apiKey = "WFoNaSbMFws9MIaRrXrSTbglBWpv60ZUZi96Pmg4XFzPY828s5DT5NATHORA2OUs";
-  
+
+    getTeamsStream() async {
+    var apiKey =
+        "WFoNaSbMFws9MIaRrXrSTbglBWpv60ZUZi96Pmg4XFzPY828s5DT5NATHORA2OUs";
+    var url = "https://www.thebluealliance.com/api/v3/event/${widget.eventCode}/teams";
+    try {
+      final response = await httpp.get(
+        Uri.parse(url),
+        headers: {'X-TBA-Auth-Key': apiKey},
+      );
+
+      if (response.statusCode == 200) {
+        setState(() {
+          allTeams = jsonDecode(response.body);
+          filteredTeams = allTeams;
+        });
+      } else {
+        print("Failed to load events: ${response.statusCode}");
+      }
+    } catch (e) {
+      print("Error fetching events: $e");
+    }
+  }
   
   
 }
